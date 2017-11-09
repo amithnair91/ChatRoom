aa159db4cab02e69d55aea14b638db77a806128b
amithnair <amith_nair@external.mckinsey.com>
2017-11-09 14:31:48 +0530

    change port number

--- app.js
+++ app.js
@@ -22,7 +22,9 @@ var files_array  = [];
 var expiryTime = 8;
 var routineTime = 1;
 
-server.listen(8282);		// server starting on port '8282'
+var port = process.env.PORT || 8080;
+
+server.listen(port);		// server starting on port '8080'
 
 // cofiguring body-parser
 app.use(bodyParser.json({	// setting json limit 	

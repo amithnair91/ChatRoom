068d3297ae7ba5229b73ea2ce29728658c7a42aa
amithnair <amith_nair@external.mckinsey.com>
2017-11-22 00:38:21 +0530

    add heroku db connections

--- dbconnection.js
+++ dbconnection.js
@@ -4,11 +4,11 @@ const initOptions = {
 };
 const pgp = require('pg-promise')(initOptions);
 const cn = {
-    host: 'localhost',
+    host: 'ec2-54-235-65-224.compute-1.amazonaws.com',
     port: 5432,
-    database: 'chatapp',
-    user: 'postgres',
-    password: ''
+    database: 'd1rv4r6kj9ev6q',
+    user: 'xeggfykrztskpo',
+    password: '8d9d1c9300debb7cb62932ae35923cd56de47fd25a2f941ef81d669b3c827838'
 };
 
 console.log('Connecting to DB')
